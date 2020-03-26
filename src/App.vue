f6ffcaabd8aa078d694d21762bce60154d7cca5f
paulloclara <paulloclara@gmail.com>
2020-03-26 19:10:13 -0300

    :rocket: Add login view

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,8 @@ export default {
 <style lang="css">
 @import url("https://fonts.googleapis.com/css?family=Roboto:400,500i,700&display=swap");
 
+/* Reset CSS */
+
 * {
   margin: 0;
   padding: 0;
@@ -30,6 +32,8 @@ export default {
   text-decoration: none;
   box-sizing: border-box;
 }
+
+/* Page CSS */
 
 body {
   background-color: #f0f0f5;
@@ -43,9 +47,38 @@ body {
   -webkit-font-smoothing: antialiased;
 }
 
+/* Global CSS */
+
 input,
 button,
 textarea {
   font: 400 14px Roboto, sans-serif;
+}
+
+.button {
+  display: inline-block;
+
+  width: 100%;
+  height: 60px;
+
+  margin-top: 16px;
+
+  border: 0;
+  border-radius: 8px;
+
+  color: #ffffff;
+  font-size: 18px;
+  font-weight: 700;
+  text-align: center;
+
+  cursor: pointer;
+  transition: filter 0.2s;
+
+  line-height: 60px;
+  background-color: #e02041;
+}
+
+.button:hover {
+  filter: brightness(90%);
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,22 @@
 <template lang="html">
-  <div class="login">
-    <h1>This is an login page</h1>
+  <div id="login">
+    <section class="content">
+      <img src="@/assets/logo.svg" alt="Be The Hero" />
+
+      <form class="form">
+        <h1 class="title">Faça seu login</h1>
+
+        <input type="text" name="id" placeholder="Sua ID" />
+        <button class="button" type="submit" name="submit">Entrar</button>
+
+        <router-link :to="{ name: 'register', params: {} }">
+          <feather type="log-in" size="16" stroke="#e02041"></feather>
+          Não tenho cadastro
+        </router-link>
+      </form>
+    </section>
+
+    <img src="@/assets/heroes.png" alt="heroes" />
   </div>
 </template>
 
@@ -10,4 +26,66 @@ export default {
 }
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+#login {
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 1120px;
+  height: 100vh;
+
+  margin: 0 auto;
+}
+
+#login .content {
+  width: 100%;
+  max-width: 350px;
+
+  margin-right: 30px;
+}
+
+#login .content .form {
+  margin-top: 100px;
+}
+
+#login .content .form .title {
+  font-size: 32px;
+  margin-bottom: 32px;
+}
+
+#login .content .form input {
+  width: 100%;
+  height: 60px;
+
+  padding: 0 24px;
+
+  border: 1px solid #dcdce6;
+  border-radius: 8px;
+
+  color: #333333;
+}
+
+#login .content .form a {
+  display: flex;
+  align-items: center;
+
+  margin-top: 40px;
+
+  color: #41414d;
+  font-size: 18px;
+  font-weight: 500;
+
+  transition: opacity 0.2s;
+}
+
+#login .content .form a svg {
+  margin-right: 8px;
+}
+
+#login .content .form a:hover {
+  opacity: 0.8;
+}
+</style>
