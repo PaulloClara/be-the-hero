c59fb5ffd85b418efb1b7969d86f99f616b9521f
paulloclara <paulloclara@gmail.com>
2020-03-26 22:50:40 -0300

    :fire: Remove nav

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template lang="html">
   <div id="app">
-    <header>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-      </div>
-    </header>
+    <header></header>
 
     <main>
       <router-view />
