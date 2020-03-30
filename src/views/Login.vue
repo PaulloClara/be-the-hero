4935d87be5acf284cfac3ac219aa5a4000119049
paulloclara <paulloclara@gmail.com>
2020-03-30 18:38:45 -0300

    :rocket: Centralize login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -65,10 +65,13 @@ export default {
   display: flex;
 
   align-items: center;
+  align-self: center;
+  justify-self: center;
   justify-content: space-between;
 
   width: 100%;
   max-width: 1120px;
+  height: 100vh;
 
   margin: 0 auto;
 }
