e524972407d357c709058db48c3a5a3f551b918b
paulloclara <paulloclara@gmail.com>
2020-03-31 12:10:51 -0300

    :rocket: Config heroku deploy

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  outputDir: "dist",
+  pwa: {
+    name: "NameGator",
+    display: "standalone",
+    themeColor: "#87c440",
+    msTileColor: "#87c440",
+    manifestOptions: {
+      short_name: "NG",
+      background_color: "#000000"
+    }
+  }
+};
