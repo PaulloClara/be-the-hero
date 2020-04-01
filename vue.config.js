209e78b9e8cffcdb7a02efbd51e0a5f9d59af029
paulloclara <paulloclara@gmail.com>
2020-03-31 22:06:54 -0300

    :rocket: Add description

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,16 @@
 module.exports = {
   outputDir: "production/dist",
   pwa: {
-    name: "BeTheHero",
+    name: "Be The Hero",
     display: "standalone",
     themeColor: "#e02041",
     msTileColor: "#e02041",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#e02041",
     manifestOptions: {
+      start_url: ".",
       short_name: "BTHero",
+      description: "Plataforma online para ajudá-lo a se tornar um herói!",
       background_color: "#000000"
     }
   }
