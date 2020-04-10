faa68dd4a4c26fb662bad1f17aea5402fccf8673
paulloclara <paulloclara@gmail.com>
2020-04-10 15:36:18 -0300

    :bug: Dev api url in production

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import Axios from "axios";
 
 export default Axios.create({
-  baseURL: process.env.VUE_APP_DEV_API_URL || process.env.VUE_APP_API_URL,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_API_URL
+      : process.env.VUE_APP_DEV_API_URL,
   timeout: 20000
 });
