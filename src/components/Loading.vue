7ca4979c308f477895655e6497853f174437ca70
paulloclara <paulloclara@gmail.com>
2020-04-12 19:19:26 -0300

    :bug: Centralize loading on small screens

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,44 +1,50 @@
-<template lang="html">
-  <div id="loading">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-</template>
-
 <script>
 export default {
   name: "Loading"
 };
 </script>
 
+<template lang="html">
+  <div class="loading">
+    <div class="">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
 <style lang="css" scoped>
-#loading {
-  display: block;
-  position: relative;
+.loading {
+  display: flex;
 
-  top: 40%;
-  left: 50%;
+  align-items: center;
+  justify-content: center;
 
+  width: 100%;
+  height: 100%;
+}
+
+.loading > div {
   width: 80px;
   height: 80px;
 }
 
-div {
+.loading div {
   animation: loading 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
   transform-origin: 40px 40px;
 }
 
-div:after {
+.loading > div > div:after {
   display: block;
   position: absolute;
 
-  content: " ";
+  content: "";
 
   width: 7px;
   height: 7px;
@@ -49,73 +55,74 @@ div:after {
   background-color: #e02041;
 }
 
-div:nth-child(1) {
+div > div:nth-child(1) {
   animation-delay: -0.036s;
 }
-div:nth-child(1):after {
+
+div > div:nth-child(1):after {
   top: 63px;
   left: 63px;
 }
 
-div:nth-child(2) {
+div > div:nth-child(2) {
   animation-delay: -0.072s;
 }
 
-div:nth-child(2):after {
+div > div:nth-child(2):after {
   top: 68px;
   left: 56px;
 }
 
-div:nth-child(3) {
+div > div:nth-child(3) {
   animation-delay: -0.108s;
 }
 
-div:nth-child(3):after {
+div > div:nth-child(3):after {
   top: 71px;
   left: 48px;
 }
 
-div:nth-child(4) {
+div > div:nth-child(4) {
   animation-delay: -0.144s;
 }
 
-div:nth-child(4):after {
+div > div:nth-child(4):after {
   top: 72px;
   left: 40px;
 }
 
-div:nth-child(5) {
+div > div:nth-child(5) {
   animation-delay: -0.18s;
 }
 
-div:nth-child(5):after {
+div > div:nth-child(5):after {
   top: 71px;
   left: 32px;
 }
 
-div:nth-child(6) {
+div > div:nth-child(6) {
   animation-delay: -0.216s;
 }
 
-div:nth-child(6):after {
+div > div:nth-child(6):after {
   top: 68px;
   left: 24px;
 }
 
-div:nth-child(7) {
+div > div:nth-child(7) {
   animation-delay: -0.252s;
 }
 
-div:nth-child(7):after {
+div > div:nth-child(7):after {
   top: 63px;
   left: 17px;
 }
 
-div:nth-child(8) {
+div > div:nth-child(8) {
   animation-delay: -0.288s;
 }
 
-div:nth-child(8):after {
+div > div:nth-child(8):after {
   top: 56px;
   left: 12px;
 }
